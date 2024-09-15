56bcdd09d94bf084b4b1e8adccd3a3084d9828e9
arielhernandezcl <ariel.dox@gmail.com>
2024-09-15 04:31:08 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.arielhernandez.net',
+  site: 'https://www.example.com',
   integrations: [mdx(), sitemap(), tailwind(), react()],
   markdown: {
     shikiConfig: {
